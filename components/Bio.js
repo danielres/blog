827e4fddbfb0edf7906eeca85a9c98c865bdfc14
Daniel Reszka <dannn.r@gmail.com>
2017-01-31 17:11:14 +0100

    customize Bio, blog title and profile-pic

--- components/Bio.js
+++ components/Bio.js
@@ -24,7 +24,7 @@ class Bio extends React.Component {
             height: rhythm(2),
           }}
         />
-        Written by <strong>{config.authorName}</strong> who lives and works in San Francisco building useful things. <a href="https://twitter.com/kylemathews">You should follow him on Twitter</a>
+        Written by <strong>{config.authorName}</strong> who lives and works in Berlin building useful things. <a href="https://twitter.com/pixarea">You should follow him on Twitter</a>
       </p>
     )
   }
