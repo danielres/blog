8aa3cc42c6ce9d200108a4937ed65bcb10f1bb34
Daniel Reszka <dannn.r@gmail.com>
2017-02-01 08:02:11 +0100

    extract BlogIndex to own commponent

---
+++ blog/index.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { Link } from 'react-router'
+import sortBy from 'lodash/sortBy'
+import { prefixLink } from 'gatsby-helpers'
+import { rhythm } from 'utils/typography'
+import Helmet from "react-helmet"
+import access from 'safe-access'
+import { config } from 'config'
+import include from 'underscore.string/include'
+import Footer from 'components/Footer'
+
+class BlogIndex extends React.Component {
+  render () {
+    const pageLinks = []
+    // Sort pages.
+    const sortedPages = sortBy(this.props.route.pages, (page) =>
+      access(page, 'data.date')
+    ).reverse()
+    sortedPages.forEach((page) => {
+      if (access(page, 'file.ext') === 'md' && !include(page.path, '/404')) {
+        const title = access(page, 'data.title') || page.path
+        pageLinks.push(
+          <li
+            key={page.path}
+            style={{
+              marginBottom: rhythm(1/4),
+            }}
+          >
+            <Link style={{boxShadow: 'none'}} to={prefixLink(page.path)}>{title}</Link>
+          </li>
+        )
+      }
+    })
+    return (
+      <div>
+        <Helmet
+          title={config.blogTitle}
+          meta={[
+            {"name": "description", "content": "Sample blog"},
+            {"name": "keywords", "content": "blog, articles"},
+          ]}
+        />
+
+        <ul>
+          {pageLinks}
+        </ul>
+
+        <Footer />
+      </div>
+    )
+  }
+}
+
+BlogIndex.propTypes = {
+  route: React.PropTypes.object,
+}
+
+export default BlogIndex
