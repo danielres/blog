a22448ffdd5a704c35a544a18d3f15ffedd07a34
Daniel Reszka <dannn.r@gmail.com>
2017-02-01 20:02:10 +0100

    add new component PageTitle

---
+++ components/PageTitle.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default styled.h3`
+  font-weight: 100;
+  text-align: center;
+  margin: 0 0 20px;
+  color: gray;
+  font-size: 100%;
+`
