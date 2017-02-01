f60dda29f7a20a84a8fa0e82f2c5be58498f831c
Daniel Reszka <dannn.r@gmail.com>
2017-02-01 17:14:18 +0100

    improve gallery page

--- pages/gallery/index.js
+++ pages/gallery/index.js
@@ -2,22 +2,30 @@ import React from 'react'
 import Footer from 'components/Footer'
 import FaFlickr from 'react-icons/lib/fa/flickr'
 
+const Img = (props) =>
+  <img
+    src={props.src}
+    alt={props.alt}
+    title={props.alt}
+    style={{borderRadius: 10}}
+  />
+
 class Gallery extends React.Component {
   render () {
     return (
       <div>
-        <img src='octo.jpg' alt='Drawing: Meet my friend Octopus' />
-        <img src='turtles.jpg' alt='Drawing: Flying Turtle' />
-        <img src='pure energy.jpg' alt='Drawing: Pure Energy' />
-        <img src='bloom.png' alt='Drawing: Krita space flower' />
-        <img src='insect-communication.jpg' alt='Photo: Insects communication macro' />
+        <Img src='octo.jpg' alt='Drawing: Meet my friend Octopus' width='900' height='693' />
+        <Img src='turtles.jpg' alt='Drawing: Flying Turtle' width='1175' height='500' />
+        <Img src='pure energy.jpg' alt='Drawing: Pure Energy' width='1024' height='707' />
+        <Img src='bloom.png' alt='Drawing: Krita space flower' width='1440' height='900' />
+        <Img src='insect-communication.jpg' alt='Photo: Insects communication macro' width='1024' height='768' />
 
-        <img src='daydreaming.jpg' alt='Drawing: Daydreaming' />
-        <img src='dead.jpg' alt='Drawing: Dead Lady' />
-        <img src='insect-activity.jpg' alt='Drawing: ' />
-        <img src='insect-autobahn.jpg' alt='Drawing: ' />
-        <img src='texture-2.jpg' alt='Drawing: ' />
-        <img src='texture-corn.jpg' alt='Drawing: ' />
+        <Img src='daydreaming.jpg' alt='Drawing: Daydreaming' width='1024' height='765' />
+        <Img src='dead.jpg' alt='Drawing: Dead Lady' width='1024' height='773' />
+        <Img src='insect-activity.jpg' alt='Drawing: ' width='1024' height='768' />
+        <Img src='insect-autobahn.jpg' alt='Drawing: ' width='1024' height='768' />
+        <Img src='texture-2.jpg' alt='Drawing: ' width='768' height='1024' />
+        <Img src='texture-corn.jpg' alt='Drawing: ' width='768' height='1024' />
 
         <br />
         <br />
