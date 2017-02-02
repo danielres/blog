bf9896907300ab2bbe24259da1614020c30e4c2d
Daniel Reszka <dannn.r@gmail.com>
2017-02-02 11:58:51 +0100

    improve Bio: add heart icon, increase picture size

--- components/Bio.js
+++ components/Bio.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router'
 import { config } from 'config'
 import { rhythm } from 'utils/typography'
 import { prefixLink } from 'gatsby-helpers'
+import IHeart from 'react-icons/lib/ti/heart'
 
 class Bio extends React.Component {
   render () {
@@ -15,22 +16,21 @@ class Bio extends React.Component {
         <img
           src={prefixLink('/static/img/profile-pic.jpg')}
           alt={`author ${config.authorName}`}
-          width="46"
-          height="46"
           style={{
             float: 'left',
             marginRight: rhythm(1/4),
+            marginTop: -5,
             marginBottom: 0,
-            width: rhythm(2),
-            height: rhythm(2),
+            width: rhythm(2.2),
+            height: rhythm(2.2),
             borderRadius: '100%',
-            border: '2px solid #f6f6f6',
+            border: '4px solid #f6f6f6',
           }}
         />
         <div
           style={{ marginLeft: 70}}
         >
-          Crafted by <strong>{config.authorName}</strong> who lives and works in Berlin building useful things.
+          Crafted with <IHeart style={{ position: 'relative', top: -2, color: 'gray'}} />  by <strong>{config.authorName}</strong> who lives and works in Berlin building useful things.
           {' '}
           <Link className='text-link' to='/about/'>You should follow him on social media</Link>.
         </div>
