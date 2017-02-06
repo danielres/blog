e7dbdea78aaf9c3794b16c3405b23dd98871e362
Daniel Reszka <dannn.r@gmail.com>
2017-02-07 00:14:11 +0100

    improve dev page: use styled-components, add contents, mobile-friendly

--- pages/dev/index.js
+++ pages/dev/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from 'styled-components'
 import Footer from 'components/Footer'
 import _IGithub from 'react-icons/lib/fa/github'
 import PageTitle from 'components/PageTitle'
@@ -29,16 +30,40 @@ const entries = [
   },
   {
     name: 'Spirales',
-    tags: ['Rails', 'RefineryCMS', 'Custom Events Engine', 'Paypal'],
-    desc: 'Offical website for an association of psychotherapists.',
+    tags: ['Rails', 'RefineryCMS', 'Custom Events Engine', 'Paypal SDK'],
+    desc: `<p>The offical website for a well-being association located near Reims, France.</p>
+            <p>This website is built with <a class="text-link" href="http://www.refinerycms.com/" target"_blank">RefineryCMS</a>, and allows the client to have complete control over the contents.</p>
+            <p>The association organizes frequent activities, therefore I developed an custom event engine as well as a secure booking
+            and online payment system that uses the Paypal API.</p>`,
     pic: 'spirales.jpg',
     links: {
       pic: 'http://www.spirales.info/',
     }
   },
   {
-    name: 'Wymeditor',
+    name: 'WYMeditor',
     pic: 'wymeditor.jpg',
+    links: {
+      pic: 'http://www.wymeditor.org/',
+    },
+    desc: `<p>
+            Back in 2005, with <a href="http://hovinne.com/" target="_blank" class="text-link">Jean-François Hovinne</a>, we launched a new type of online editor,
+            challenging the very concept of existing WYSYWIG editors, and allowing for higher-quality and more reliable output.
+           </p>
+           <p>
+             WYMeditor is still improving and being used and integrated in a
+             long and growing list of high-profile websites (see <a href="http://www.wymeditor.org/" class="text-link" target="_blank">Applications using WYMeditor</a>),
+             such as
+             <a href="http://www.kickstarter.com/" target="_blank" class="text-link">Kickstarter</a> and
+             <a href="http://www.policystat.com/" target="_blank" class="text-link">PolicyStat</a>, and content management systems like
+             <a href="http://www.refinerycms.com/" target="_blank" class="text-link">RefineryCMS</a>.
+           </p>
+           <p>
+             I don't contribute anymore to WYMeditor, the project is now maintained by
+             <a href="https://github.com/winhamwr" target="_blank" class="text-link">Wes Winham</a>
+             and other contributors (who are doing an amazing job).
+           </p>
+           `,
   },
   {
     name: 'Nathalie Champougny',
@@ -91,47 +116,125 @@ const entries = [
   // },
 ]
 
-const entryStyle = { padding: '20px 20px 1px', background: 'white', marginBottom: 20, borderRadius: 10, overflow: 'hidden'}
-const entryTitleStyle = { fontWeight: 'normal', marginTop: 0}
-const entryTagsStyle = { textTransform: 'uppercase'}
-const entryTagStyle = { marginLeft: '1.5em', fontSize: '80%', color: 'gray'}
+
+const Entry = styled.div`
+  padding: 20px 20px 1px;
+  background: white;
+  margin-bottom: 20px;
+  border-radius: 10px;
+  overflow: hidden;
+  `
+
+const EntryTitle = styled((p) =>
+  <h3 className={p.className}>
+    <span
+      className='text'
+      dangerouslySetInnerHTML={{ __html:  p.text}}
+    />
+    {p.tags  &&
+      <small className='tags'>
+        {p.tags.map((t) => <span className='tag'>{t}</span>)}
+      </small>
+    }
+  </h3>
+  )`
+  font-weight: normal;
+  margin-top: 0.2em;
+  margin-bottom: 0.8em;
+  .text {
+    margin-right: 1.5em;
+    display: inline-block;
+    margin-bottom: 0.5em;
+  }
+  .tags {
+    display: inline-block;
+    text-transform: uppercase;
+    line-height: 1.2;
+  }
+  .tag {
+    min-width: 5em;
+    text-align: center;
+    display: inline-block;
+    padding: 0 0.5em;
+    font-size: 80%;
+    color: gray;
+    border-left: 1px solid #bbb;
+    border-right: 1px solid #bbb;
+    margin-right: -1px;
+    &:last-child {
+    }
+  }
+  `
+
+const EntryContents = styled(({e, className}) => {
+  console.log(e)
+  const hasPic = e.pic ? 'hasPic' : ''
+  return <div className={`${className}`}>
+      <div className='pic'>
+        {e.links && e.links.pic
+          ? <a href={e.links.pic} tilte='Visit website' target='_blank'>
+              <img src={e.pic} alt={e.name} title={`Project: ${e.name}`} />
+            </a>
+          : e.pic &&
+            <img src={e.pic} alt={e.name} title={`Project: ${e.name}`} />
+        }
+      </div>
+      <div className={hasPic}>
+        {e.desc && <div className='desc' dangerouslySetInnerHTML={{ __html: e.desc }} /> }
+
+        {e.links && e.links.github &&
+          <p className='links'>
+            <IGithub />
+            {' '}
+            <a className='text-link' href={`https://github.com/${e.links.github}`}>{e.links.github}</a>
+          </p>
+        }
+      </div>
+    </div>
+  })`
+    .pic {
+      @media screen and (min-width: 480px){
+        float: left;
+        width: 40%;
+      }
+      img {
+        border: 4px solid transparent;
+        border-radius: 10px;
+        box-sizing: content-box;
+        transition-duration: 0.4s;
+        margin-left: -4px;
+        &:hover {
+          border: 4px solid #58BCFF;
+          background: #58BCFF;
+        }
+      }
+    }
+    .hasPic .desc,
+    .hasPic .links {
+      @media screen and (min-width: 480px){
+        float: right;
+        width: 57%;
+      }
+    }
+    .desc,
+    .links {
+      line-height: 1.6;
+      p {
+        margin-bottom: 1.4em
+      }
+    }
+  `
 
 class Gallery extends React.Component {
   render () {
     return (
       <div>
         <PageTitle>Programming</PageTitle>
-
         {entries.map((e) =>
-          <div style={entryStyle} key={e.name}>
-            <h3 style={entryTitleStyle}>
-              <span dangerouslySetInnerHTML={{ __html: e.name }} />
-              {e.tags  && <small style={entryTagsStyle}>{e.tags.map((t) => <span style={entryTagStyle}>{t}</span>)}</small>}
-            </h3>
-
-            <div style={{float: 'left', marginRight: 40, width: 'calc(50% - 20px)'}}>
-              {e.links && e.links.pic
-                ? <a href={e.links.pic} tilte='Visit website' target='_blank'>
-                    <img src={e.pic} alt={e.name} title={`Project: ${e.name}`} style={{borderRadius: 5}} />
-                  </a>
-                : e.pic &&
-                  <img src={e.pic} alt={e.name} title={`Project: ${e.name}`} style={{borderRadius: 5}} />
-              }
-            </div>
-            <div></div>
-            {e.desc &&
-              <p dangerouslySetInnerHTML={{ __html: e.desc }} />
-            }
-
-            {e.links && e.links.github &&
-              <p>
-                <IGithub />
-                {' '}
-                <a className='text-link' href={`https://github.com/${e.links.github}`}>{e.links.github}</a>
-              </p>
-            }
-
-          </div>
+          <Entry key={e.name}>
+            <EntryTitle text={e.name} tags={e.tags} />
+            <EntryContents e={e} />
+          </Entry>
         )}
         <br />
         <Footer />
