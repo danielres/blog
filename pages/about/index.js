8dc6f60e441c4b64eecfd9baa45eaa43c2171121
Daniel Reszka <dannn.r@gmail.com>
2017-02-09 19:19:01 +0100

    extract SocialLinks from about page

--- pages/about/index.js
+++ pages/about/index.js
@@ -5,13 +5,8 @@ import styled from 'styled-components'
 import Footer from 'components/Footer'
 import _Envelope from 'react-icons/lib/fa/envelope'
 import _Check from 'react-icons/lib/fa/check'
-import FaGithub from 'react-icons/lib/fa/github'
-import FaFacebook from 'react-icons/lib/fa/facebook'
-import FaTwitter from 'react-icons/lib/fa/twitter'
-import FaLinkedin from 'react-icons/lib/fa/linkedin'
-import FaFlickr from 'react-icons/lib/fa/flickr'
-import MdInfo from 'react-icons/lib/md/info'
 import PageTitle from 'components/PageTitle'
+import SocialLinks from './SocialLinks'
 
 
 const I = styled.span`
@@ -59,40 +54,6 @@ const BorderLeft = styled.div`
     margin-bottom:20px;
 `
 
-const S = styled.span`
-  @media screen and (min-width: 480px) {
-    position: relative;
-    left: -29px;
-  }
-  margin-top: 20px;
-  display: inline-block;
-  text-align: center;
-  color: #f6f6f6;
-  background: #999;
-  border-radius: 20%;
-  padding: 5px 10px 8px;
-  line-height: 45px;
-  width: 60px;
-  margin-right: 15px;
-  font-size: 40px;
-  transition-duration: 0.4s;
-    &:hover {
-      background: #5790C4;
-      color: white;
-    }
-  a {
-    color: inherit;
-  }
-  position: relative;
-  transition-duration: 0.2s;
-  top: 0;
-  box-shadow: 0 0 0 rgba(0,0,0,0);
-  &:hover {
-    top: -5px;
-    box-shadow: -0px 10px 10px rgba(0,0,0,0.2);
-  }
-`
-
 const ContactLink = styled(
   (p) =>
     <a
@@ -120,8 +81,6 @@ const ContactLink = styled(
       border-bottom: 1px solid #007acc;
     }
   `
-
-
 
 class Page extends React.Component {
   render () {
@@ -155,43 +114,7 @@ class Page extends React.Component {
           VAT: DE 298 753 316
         </BorderLeft>
 
-        <S>
-          <a title="github.com/danielres" href="https://www.github.com/danielres" target="_blank" rel="external">
-            <FaGithub />
-          </a>
-        </S>
-
-        <S>
-          <a title="twitter.com/pixarea" href="https://twitter.com/pixarea" target="_blank" rel="external">
-            <FaTwitter />
-          </a>
-        </S>
-
-        <S>
-          <a title="facebook.com/daniel.reszka.58" href="https://www.facebook.com/daniel.reszka.58" target="_blank" rel="external">
-            <FaFacebook />
-          </a>
-        </S>
-
-        <S>
-          <a title="linkedin.com/in/danielres" href="https://be.linkedin.com/in/danielres" target="_blank" rel="external">
-            <FaLinkedin />
-          </a>
-        </S>
-
-        <S>
-          <a title="flickr.com/photos/pixarea" href="https://www.flickr.com/photos/pixarea/albums" target="_blank" rel="external">
-            <FaFlickr />
-          </a>
-        </S>
-
-        <S>
-          <a title="about.me/daniel.r" href="https://about.me/daniel.r" target="_blank" rel="external">
-            <MdInfo />
-          </a>
-        </S>
-
-
+        <SocialLinks />
       </div>
     )
   }
